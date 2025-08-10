a8087c30084bd48d09317a07cb4c4a6cc513a426
Szymon Lenard <szymon.lenard@icloud.com>
2025-08-10 13:05:18 +0200

    Creating a project

---
+++ src/electron/main.js
@@ -0,0 +1,12 @@
+﻿import {app, BrowserWindow} from 'electron';
+import path from 'path';
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        autoHideMenuBar: true,
+    });
+
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist/index.html"));
+})
